193292a20bd98fd9d7e49b7b2a6cdd940c3de385
Suzanne Cabral <suzannecabral@live.com>
2020-08-13 10:33:55 -0700

    intial commit, #1 lecture practice

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(name, cost, category){
-    /* Code here */
+function createMenuItem(name, price, category){
+    return {name, price, category};
 }
+
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
-
+console.log(createMenuItem("Tacos", 11, "Lunch"));
+console.log(createMenuItem("Fruit Cup", 3, "Snacks"));
+console.log(createMenuItem("Curry", 10, "Dinner"));
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
